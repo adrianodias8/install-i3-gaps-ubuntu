d6888005bab0d6688b03d9bdb55535a8ef6271cd
adrianodias8 <adrianodias1994@gmail.com>
2019-01-25 15:28:25 +0100

    update install-i3.sh
    work in progress

--- install-i3.sh
+++ install-i3.sh
@@ -3,16 +3,16 @@
 ###### Place the default wallpaper in $HOME directory
 cp wallpaper.jpg ~/.wallpaper.jpg
 
-###### Install Dependencies
-sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
+###### Install Dependencies (i3-gaps + polybar)
+sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev cmake cmake-data libcairo2-dev libxcb-ewmh-dev libxcb-image0-dev pkg-config python-xcbgen libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev
 
 cd /tmp
 
-# clone the repository
+###### Get i3-gaps
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
 
-# compile & install
+###### Compile & Install
 autoreconf --force --install
 rm -rf build/
 mkdir -p build && cd build/
@@ -31,12 +31,7 @@ git clone https://github.com/jaagr/polybar.git
 cd polybar && ./build.sh
 
 ######more stuff
-sudo apt install -y i3status i3lock i3blocks suckless-tools nitrogen rofi  urxvt-unicode clipit thunar compton arc-theme arandr
-
-##### Get and install infinality (better font rendering)
-add-apt-repository ppa:no1wantdthisname/ppa -y
-apt-get update
-apt-get install -y fontconfig-infinality
+sudo apt install -y i3status i3lock i3blocks suckless-tools nitrogen rofi clipit thunar compton arc-theme arandr
 
 ###### Make .fonts directory if not already available
 mkdir ~/.fonts
@@ -50,7 +45,6 @@ rm -rf YosemiteSanFranciscoFont
 cd /tmp
 ###### Get and install Font Awesome Web Font
 git clone https://github.com/FortAwesome/Font-Awesome.git
-cp -v Font-Awesome/fonts/*.ttf ~/.fonts
+cp -v Font-Awesome/webfonts/*.ttf ~/.fonts
 rm -rf Font-Awesome
-
 
