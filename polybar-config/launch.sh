ecbaa2bc4a2b5ca503ceedbe6f9b8e3330cbb05e
Adriano Dias <adrianodias1994@gmail.com>
2019-02-12 16:41:35 +0100

    polybar settings and fix conky

--- polybar-config/launch.sh
+++ polybar-config/launch.sh
@@ -6,8 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar bar1 &
-polybar bar2 &
+# Launch bar1
+polybar bar1
 
 echo "Bars launched..."
